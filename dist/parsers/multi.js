6838d335fe270983ab70e72e4332cd804199e3d1
Yoon, Chi Ho <cyoon@microstrategy.com>
2018-04-26 12:04:03 -0400

    Update parser regex

--- dist/parsers/multi.js
+++ dist/parsers/multi.js
@@ -13,23 +13,20 @@ exports['default'] = {
     var match = null;
     var failedSpecs = new Set();
     var testsOutput = output.split('------------------------------------');
-    var RESULT_REG = /SUCCESS/g;
+    var RESULT_REG = /(spec(.+)SUCCESS(.)in)|(spec(.+)INCOMPLETE(.)in)/g;
     var SPECFILE_REG = /.+Specs:\s(.*\.(js|coffee))/g;
     testsOutput.forEach(function (test) {
       var specfile = undefined;
       var result = 'failed';
       // retrieve specfile from run
-      console.log('Test: ' + test);
       while (match = SPECFILE_REG.exec(test)) {
         // eslint-disable-line no-cond-assign
         specfile = match[1];
-        console.log('Spec file: ' + specfile);
       }
       // check for string ' SUCCESS ' and then marks the test as passed
       while (match = RESULT_REG.exec(test)) {
         // eslint-disable-line no-cond-assign
         result = 'passed';
-        console.log('Match: ' + match);
       }
       if (specfile && result === 'failed') {
         if (!/node_modules/.test(specfile)) {
--- src/parsers/multi.js
+++ src/parsers/multi.js
@@ -5,21 +5,18 @@ export default {
     let match = null
     let failedSpecs = new Set()
     let testsOutput = output.split('------------------------------------')
-    let RESULT_REG = /SUCCESS/g
+    let RESULT_REG = /(spec(.+)SUCCESS(.)in)|(spec(.+)INCOMPLETE(.)in)/g
     let SPECFILE_REG = /.+Specs:\s(.*\.(js|coffee))/g
     testsOutput.forEach(function (test) {
       let specfile
       let result = 'failed'
       // retrieve specfile from run
-      console.log(`Test: ${test}`);
       while (match = SPECFILE_REG.exec(test)) { // eslint-disable-line no-cond-assign
         specfile = match[1]
-        console.log(`Spec file: ${specfile}`);
       }
       // check for string ' SUCCESS ' and then marks the test as passed
       while (match = RESULT_REG.exec(test)) { // eslint-disable-line no-cond-assign
         result = 'passed'
-        console.log(`Match: ${match}`);
       }
       if (specfile && result === 'failed') {
         if (!/node_modules/.test(specfile)) {
